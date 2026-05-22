b38ce05835ab008c1cd674904ee90b4d15c2267a
leticia040124 <lemos.ozorio05@aluno.ifce.edu.br>
2026-05-22 12:10:24 -0300

    atividade-08

---
+++ avaliacoes/avaliacao-08/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+
+const githubClientId = 'Ov23liY7RIU40acfEo4q';
+const githubClientSecret = 'ef90faa864e258db62813a4427824fa4eb16881d';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
